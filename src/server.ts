bae26c407f2ad998a4ff899b465f5537f289dbe4
Saturu-Gojo <prashasth068@gmail.com>
2026-07-28 07:47:10 +0530

    add express server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
+import app from "./app.js";
 import { Config } from "./config/index.js";
 
-console.log(Config.PORT);
+const startServer = () => {
+  const PORT = Config.PORT || 5555;
+  try {
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+startServer();
